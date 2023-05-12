570fb66bf2c314d5220044650525b1f07ea87ddd
lucas.sproule <lucas.sproule.42@gmail.com>
2023-05-12 17:28:23 -0400

    lessseee

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -48,32 +48,7 @@ return {
   },
   {
     "nvim-treesitter/playground",
-    event = "User Astrofile",
+    event = "BufEnter",
     enable = true,
-    config = {
-      function()
-        local status_ok, configs = pcall(require, "nvim-treesitter.configs")
-        configs.setup {
-          playground = {
-            enable = true,
-            disable = {},
-            updatetime = 25,         -- Debounced time for highlighting nodes in the playground from source code
-            persist_queries = false, -- Whether the query persists across vim sessions
-            keybindings = {
-              toggle_query_editor = "o",
-              toggle_hl_groups = "i",
-              toggle_injected_languages = "t",
-              toggle_anonymous_nodes = "a",
-              toggle_language_display = "I",
-              focus_language = "f",
-              unfocus_language = "F",
-              update = "R",
-              goto_node = "<cr>",
-              show_help = "?",
-            },
-          },
-        }
-      end,
-    },
   },
 }
