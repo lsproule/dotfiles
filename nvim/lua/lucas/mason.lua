eaf9298c33166fdb77491620de31122c573721df
lsproule <lucas.sproule.42@gmail.com>
2023-09-27 01:36:46 -0400

    fix lua lsp

--- nvim/lua/lucas/mason.lua
+++ nvim/lua/lucas/mason.lua
@@ -134,34 +134,36 @@ vim.api.nvim_create_autocmd("LspAttach", {
 })
 
 
-
-
-
 lspconfig.lua_ls.setup {
-  on_init = function(client)
-    local path = client.workleader_folders[1].name
-    if not vim.loop.fs_stat(path..'/.luarc.json') and not vim.loop.fs_stat(path..'/.luarc.jsonc') then
-      client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
-        Lua = {
-          runtime = {
-            -- Tell the language server which version of Lua you're using
-            -- (most likely LuaJIT in the case of Neovim)
-            version = 'LuaJIT'
-          },
-          -- Make the server aware of Neovim runtime files
-          workleader = {
-            checkThirdParty = false,
-            -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
-            library = vim.api.nvim_get_runtime_file("", true)
-          }
-        }
-      })
-
-      client.notify("workleader/didChangeConfiguration", { settings = client.config.settings })
-    end
-    return true
-  end
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using
+        -- (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {
+          'vim',
+          'require'
+        },
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
 }
+
+
+
+
 
 lspconfig.tsserver.setup({
 	settings = {
