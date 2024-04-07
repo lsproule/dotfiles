0de260b8dc4ac6e0d5612795a923176970e41b26
lucas sproule <lucas.sproule.42@gmail.com>
2024-04-07 03:27:00 -0400

    guess who did it? damn right

---
+++ scripts/aliases.zsh
@@ -0,0 +1,14 @@
+alias vim="nvim"
+alias nv="nvim"
+alias wrk="cd ~/wrk/"
+alias pbcopy="xclip -sel clip"
+alias luamake=/home/lucas/lua-language-server/3rd/luamake/luamake
+alias c="clear"
+alias ls="eza --icons "
+alias cd="z"
+alias x=exit
+alias ff=". ranger && nvim ."
+alias woman='vim +"Telescope man_pages sections=1,2,3,4,5,6,7,8,9"'
+alias tma="tmux attach-session -t"
+alias tml="tmux list-sessions"
+alias tmn="tmux new-session -s"
