5a5c22577043c98e9844e856016c5e1d66d28d3e
lucas <lucas@urmom.urmom>
2024-07-01 21:40:44 -0400

    update repos

--- scripts/aliases.zsh
+++ scripts/aliases.zsh
@@ -3,10 +3,23 @@ alias nv="nvim"
 alias wrk="cd ~/wrk/"
 alias c="clear"
 alias ls="eza --icons "
-alias cd="z"
 alias x=exit
 alias ff=". ranger && nvim ."
 alias woman='vim +"Telescope man_pages sections=1,2,3,4,5,6,7,8,9"'
 alias tma="tmux attach-session -t"
 alias tml="tmux list-sessions"
 alias tmn="tmux new-session -s"
+if command -v zoxide &> /dev/null; then
+  alias cd="z"
+fi
+
+function init_latex(){
+  if [[ -z $1 ]]; then
+    echo "Please provide a directory to initialize"
+    return
+  fi
+  if [[  $1 != "." ]]; then
+    mkdir -p $1
+  fi
+  cp -r ~/dotfiles/latex/* $1
+}
